de8b177937426089dffa6cb1abb2d66ed296aac8
Amit Singh <singhamitchs19@gmail.com>
2025-10-27 19:19:36 +0530

    added user essentials

--- Backend/app.js
+++ Backend/app.js
@@ -5,9 +5,13 @@ const express = require('express')
 const app = express()
 const userRoutes = require('./routes/user.routes');
 const connectToDb = require('./db/db');
+const cookieParser = require('cookie-parser');
 connectToDb();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get('/' , (req,res) => {
     res.send("Hello World")
