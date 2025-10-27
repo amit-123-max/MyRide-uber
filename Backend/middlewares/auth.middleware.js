7dfdb3e92f150292bac1282b55a88c947ba92b46
Amit Singh <singhamitchs19@gmail.com>
2025-10-27 23:24:51 +0530

    user logout

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -1,6 +1,7 @@
 const userModel = require('../models/user.model');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const blackListTokenModel = require('../models/blacklistToken.model');
 
 module.exports.authUser = async (req, res, next) => {
     const token = req.cookies.token || req.headers.authorization?.split(' ')[ 1 ];
@@ -10,11 +11,11 @@ module.exports.authUser = async (req, res, next) => {
     }
 
 
-    // const isBlacklisted = await blackListTokenModel.findOne({ token: token });
+    const isBlacklisted = await blackListTokenModel.findOne({ token: token });
 
-    // if (isBlacklisted) {
-    //     return res.status(401).json({ message: 'Unauthorized' });
-    // }
+    if (isBlacklisted) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
 
     try {
 
